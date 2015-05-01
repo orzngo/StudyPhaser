6ff5425cca8e54136bcfcbbd8b606bf888b586bc
orzngo <orzlabo@gmail.com>
2015-05-01 21:04:48 +0900

    gulp周り少し追加

---
+++ gulpfile.ts
@@ -0,0 +1,5 @@
+/// <reference path="./typings/gulp/gulp.d.ts"/>
+/// <reference path="./typings/gulp-typescript/gulp-typescript.d.ts"/>
+
+import gulp = require("gulp");
+import ts = require("gulp-typescript");
